4f40ff2ece351ddf65fd048bb2957fa04b704245
Romanchik Aleksey <romanchikaleksey@example.com>
2022-02-02 17:26:00 +0300

    Add timer

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (target && target.classList.contains("tabheader__item")) {
             tabHeaderItems.forEach((item, i) => {
-                if(item == target){
+                if (item == target) {
                     hideTabs();
                     showTab(i);
                 }
@@ -37,4 +37,53 @@ document.addEventListener("DOMContentLoaded", () => {
 
     hideTabs();
     showTab();
+
+
+    const deadLine = '2022-02-15',
+        timer = document.querySelector(".timer");
+
+    const getInterval = (endTime) => {
+        const total = Date.parse(endTime) - Date.now(),
+            days = Math.floor(total / 1000 / 60 / 60 / 24),
+            hours = Math.floor(total / 1000 / 60 / 60 % 24),
+            minutes = Math.floor(total / 1000 / 60 % 60),
+            seconds = Math.floor(total / 1000 % 60);
+
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    };
+
+    const addZero = (num) => {
+        return (num >= 0 && num < 10) ? `0${num}` : num;
+    };
+
+    const renderTimer = (endTime, timerElement) => {
+        const days = timerElement.querySelector("#days"),
+            hours = timerElement.querySelector("#hours"),
+            minutes = timerElement.querySelector("#minutes"),
+            seconds = timerElement.querySelector("#seconds"),
+            intervalId = setInterval(changeTimer, 1000);
+
+        changeTimer();
+
+        function changeTimer() {
+            const interval = getInterval(endTime);
+
+            days.textContent = addZero(interval.days);
+            hours.textContent = addZero(interval.hours);
+            minutes.textContent = addZero(interval.minutes);
+            seconds.textContent = addZero(interval.seconds);
+
+            if (interval.total <= 0) {
+                clearInterval(intervalId);
+            }
+        }
+    };
+
+    renderTimer(deadLine, timer);
 });
